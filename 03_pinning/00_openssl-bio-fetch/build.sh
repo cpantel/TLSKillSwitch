7dcec5e259d6ac588fa58b43248f4ed6fb58b4d0
Carlos Pantelides <carlos.pantelides@gmail.com>
2017-12-09 17:25:00 -0300

    better comparisson between versions

--- 03_pinning/00_openssl-bio-fetch/build.sh
+++ 03_pinning/00_openssl-bio-fetch/build.sh
@@ -1,2 +1,8 @@
-cc    -g3 -ggdb -O0 -DDEBUG=1 -std=c99 -Wall -Wextra -Wconversion -Wformat -Wformat=2 -Wformat-security -Wno-deprecated-declarations -Wno-unused-function  -I /usr/include/openssl/ -I /usr/include openssl-bio-fetch.c -o openssl-bio-fetch.exe   -lssl -lcrypto
+cc    -g3 -ggdb -O0  -std=c99 -Wall -Wextra -Wconversion -Wformat -Wformat=2 \
+-Wformat-security -Wno-deprecated-declarations -Wno-unused-function  \
+-I /usr/include/openssl/ \
+-I /usr/include \
+openssl-bio-fetch.c ../../tls_shims/common.c \
+-o openssl-bio-fetch.exe \
+-lssl -lcrypto
 
--- 03_pinning/01_openssl-bio-fetch/openssl-bio-fetch.c
+++ 03_pinning/01_openssl-bio-fetch/openssl-bio-fetch.c
@@ -1,12 +1,9 @@
-
 #include "openssl-bio-fetch.h"
+#include "../../tls_shims/common.h"
 
 int verify_callback(int preverify, X509_STORE_CTX* x509_ctx);
 
 void init_openssl_library(void);
-void print_cn_name(const char* label, X509_NAME* const name);
-void print_san_name(const char* label, X509* const cert);
-void print_error_string(unsigned long err, const char* const label);
 
 /* Cipher suites, https://www.openssl.org/docs/apps/ciphers.html */
 const char* const PREFERRED_CIPHERS = "HIGH:!aNULL:!kRSA:!SRP:!PSK:!CAMELLIA:!RC4:!MD5:!DSS";
@@ -339,106 +336,6 @@ int main(int argc, char* argv[])
 }
 
 
-
-void print_cn_name(const char* label, X509_NAME* const name)
-{
-    int idx = -1, success = 0;
-    unsigned char *utf8 = NULL;
-    
-    do
-    {
-        if(!name) break; /* failed */
-        
-        idx = X509_NAME_get_index_by_NID(name, NID_commonName, -1);
-        if(!(idx > -1))  break; /* failed */
-        
-        X509_NAME_ENTRY* entry = X509_NAME_get_entry(name, idx);
-        if(!entry) break; /* failed */
-        
-        ASN1_STRING* data = X509_NAME_ENTRY_get_data(entry);
-        if(!data) break; /* failed */
-        
-        int length = ASN1_STRING_to_UTF8(&utf8, data);
-        if(!utf8 || !(length > 0))  break; /* failed */
-        
-        fprintf(stdout, "  %s: %s\n", label, utf8);
-        success = 1;
-        
-    } while (0);
-    
-    if(utf8)
-        OPENSSL_free(utf8);
-    
-    if(!success)
-        fprintf(stdout, "  %s: <not available>\n", label);
-}
-
-void print_san_name(const char* label, X509* const cert)
-{
-    int success = 0;
-    GENERAL_NAMES* names = NULL;
-    unsigned char* utf8 = NULL;
-    
-    do
-    {
-        if(!cert) break; /* failed */
-        
-        names = X509_get_ext_d2i(cert, NID_subject_alt_name, 0, 0 );
-        if(!names) break;
-        
-        int i = 0, count = sk_GENERAL_NAME_num(names);
-        if(!count) break; /* failed */
-        
-        for( i = 0; i < count; ++i )
-        {
-            GENERAL_NAME* entry = sk_GENERAL_NAME_value(names, i);
-            if(!entry) continue;
-            
-            if(GEN_DNS == entry->type)
-            {
-                int len1 = 0, len2 = -1;
-                
-                len1 = ASN1_STRING_to_UTF8(&utf8, entry->d.dNSName);
-                if(utf8) {
-                    len2 = (int)strlen((const char*)utf8);
-                }
-                
-                if(len1 != len2) {
-                    fprintf(stderr, "  Strlen and ASN1_STRING size do not match (embedded null?): %d vs %d\n", len2, len1);
-                }
-                
-                /* If there's a problem with string lengths, then     */
-                /* we skip the candidate and move on to the next.     */
-                /* Another policy would be to fails since it probably */
-                /* indicates the client is under attack.              */
-                if(utf8 && len1 && len2 && (len1 == len2)) {
-                    fprintf(stdout, "  %s: %s\n", label, utf8);
-                    success = 1;
-                }
-                
-                if(utf8) {
-                    OPENSSL_free(utf8), utf8 = NULL;
-                }
-            }
-            else
-            {
-                fprintf(stderr, "  Unknown GENERAL_NAME type: %d\n", entry->type);
-            }
-        }
-
-    } while (0);
-    
-    if(names)
-        GENERAL_NAMES_free(names);
-    
-    if(utf8)
-        OPENSSL_free(utf8);
-    
-    if(!success)
-        fprintf(stdout, "  %s: <not available>\n", label);
-    
-}
-
 int verify_callback(int preverify, X509_STORE_CTX* x509_ctx)
 {
     /* For error codes, see http://www.openssl.org/docs/apps/verify.html  */
@@ -486,13 +383,4 @@ int verify_callback(int preverify, X509_STORE_CTX* x509_ctx)
 #else
     return preverify;
 #endif
-}
-
-void print_error_string(unsigned long err, const char* const label)
-{
-    const char* const str = ERR_reason_error_string(err);
-    if(str)
-        fprintf(stderr, "%s\n", str);
-    else
-        fprintf(stderr, "%s failed: %lu (0x%lx)\n", label, err, err);
 }
