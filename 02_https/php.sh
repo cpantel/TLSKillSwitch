a372e2f78f41dd5709905aad0674f9202a4a247f
Carlos Pantelides <carlos.pantelides@gmail.com>
2017-12-10 20:57:28 -0300

    fixed hardcoded clarin

--- 02_https/php.sh
+++ 02_https/php.sh
@@ -3,7 +3,7 @@ read -p "PHP a falso  $1 con cada shim"
 ping "$1" -c 1 | grep from
 echo
 
-php -r 'echo file_get_contents("https://www.clarin.com");'
+php -r 'echo file_get_contents("https://'$1'");'
 
 read -p "............................................................................................"
 LD_PRELOAD=../tls_shims/shim_v1.so  php -r 'echo file_get_contents("https://'$1'");'
